eef478860fddc94380464705faa257e19e43fd9f
khaccao <nguyenkhaccao1@gmail.com>
2026-04-04 16:54:57 +0700

    feat: restrict swagger to customer group on dedicated subdomain

--- AciPlatform.Api/Program.cs
+++ AciPlatform.Api/Program.cs
@@ -127,8 +127,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Full API v1");
     c.SwaggerEndpoint("/swagger/customer/swagger.json", "Customer API v1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Full API v1");
     c.RoutePrefix = "swagger";
 });
 
