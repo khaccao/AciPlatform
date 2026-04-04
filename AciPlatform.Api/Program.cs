5adf2ace987c559cc744135a357678085e58e4b9
khaccao <nguyenkhaccao1@gmail.com>
2026-04-04 16:29:34 +0700

    chore: enable swagger in all environments

--- AciPlatform.Api/Program.cs
+++ AciPlatform.Api/Program.cs
@@ -109,11 +109,12 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AciPlatform API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 
